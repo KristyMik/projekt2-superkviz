39b8b39be39b269488bffbd72967a4a6b2b47e8d
KristyMik <mikulcova.kristyna@gmail.com>
2022-04-11 01:05:05 +0200

    přidány otázky, odpovědi a vyhodnocování správnosti

--- kviz.js
+++ kviz.js
@@ -1,101 +1,136 @@
 // vytvoreni objektu otazka
 let poleOtazek = [
   {
-      otazka: 'Kolik je v roce tydnu?',
-      obrazek: 'obrazky/pivo.jpg',
-      poleMoznychOdpovedi: [1,2,3],
-      indexSpravneOdpovedi: [0],
-    },
-    
-     {
-        otazka: 'kkk',
-        obrazek: '',
-        poleMoznychOdpovedi: [1,2,3],
-        indexSpravneOdpovedi: [1],
-      },
-    
-       {
-        otazka: 'nnnn',
-        obrazek: '',
-        poleMoznychOdpovedi: [1,2,3],
-        indexSpravneOdpovedi: [3],
-      }
-  ]
-let odpovedi = document.getElementById('odpovedi');
+    otazka: 'Kolik je v roce tydnu?',
+    obrazek: 'obrazky/pivo.jpg',
+    poleMoznychOdpovedi: ['52', '54', '54'],
+    indexSpravneOdpovedi: [0],
+  },
+
+  {
+    otazka: 'Kolik ma tyden minut?',
+    obrazek: 'obrazky/moncicak.jpg',
+    poleMoznychOdpovedi: ['10 020', '10 080', '10 140'],
+    indexSpravneOdpovedi: [1],
+  },
+
+  {
+    otazka: 'Jak dlouho obiha Mesic okolo Zeme?',
+    obrazek: 'obrazky/snehurka.jpg',
+    poleMoznychOdpovedi: ['28 dni', '30,5 dne', '29,5 dne'],
+    indexSpravneOdpovedi: [2],
+  }
+]
+
 let kviz = document.querySelector('.kviz');
+let odpovedi = document.getElementById('odpovedi');
+
+let pocitaniOtazek = document.createElement('div');
+pocitaniOtazek.id = "poradi";
 
 let otazka = document.createElement('div');
 otazka.id = "otazka";
 
-//let otazka = document.getElementById('#otazka');
-otazka.innerHTML = poleOtazek[0].otazka;
 let moznosti = document.createElement('div');
 moznosti.id = "moznosti";
-
-
-
-
-
 
 let foto = document.createElement('img');
 foto.classList.add("foto");
 foto.id = ("obrazek");
-foto.src = poleOtazek[0].obrazek;
 
 
-//kviz.appendChild(otazka);
-
+kviz.appendChild(pocitaniOtazek);
 kviz.appendChild(otazka);
 kviz.appendChild(moznosti);
 moznosti.appendChild(odpovedi);
 kviz.appendChild(foto);
-//otazka.appendChild(odpovedi);
-const vsechnyOdpovedi = document.querySelectorAll('#odpovedi');
-
-/* vytvoreni divu pro otazky
-
-for (let i = 0 ; i < poleOtazek.length; i++){
-  
-  if (click == true)
-
-  
-  
-}*/
 
 
-
-vsechnyOdpovedi.forEach((a) => {
-  
-  a.addEventListener('click', (ukazDalsiOtazku));
+function hrajKviz(i) {
 
 
+  let cisloOtazky = i + 1;
 
-})
+  pocitaniOtazek.innerHTML = ('Otázka ' + cisloOtazky + '/' + poleOtazek.length);
+  foto.src = poleOtazek[i].obrazek;
 
+  otazka.innerHTML = poleOtazek[i].otazka;
+  vypisMozneOdpovedi(i);
+  kliknutiNaOdpoved();
 
-
-function ukazDalsiOtazku(akce){
-
-  let odpovezeno = akce.target;
-  let spravneOdpovedi = 0;
-  let indexKliknuteOdpovedi = odpovezeno.dataset.odpoved;
-  let indexSpravneOdpovedi = poleOtazek[0].indexSpravneOdpovedi;
-
-  console.log('klikla jsem na: ' + indexKliknuteOdpovedi + ' a '+ indexSpravneOdpovedi);
-
-
-  if(indexKliknuteOdpovedi==indexSpravneOdpovedi){
-
-    spravneOdpovedi++;
-    console.log(spravneOdpovedi); 
-
-  }
 
 }
 
- 
- 
 
- 
+let odp1 = document.getElementById('od1');
+let odp2 = document.getElementById('od2');
+let odp3 = document.getElementById('od3');
 
+hrajKviz(0);
+
+
+function vypisMozneOdpovedi(i) {
+
+
+  odp1.innerHTML = poleOtazek[i].poleMoznychOdpovedi[0];
+  odp2.innerHTML = poleOtazek[i].poleMoznychOdpovedi[1];
+  odp3.innerHTML = poleOtazek[i].poleMoznychOdpovedi[2];
+
+  //iteruj(i);
+}
+
+function iteruj(i) {
+
+  if (i < poleOtazek.length) {
+    i++;
+    hrajKviz(i);
+    console.log('i: ' + i);
+
+  }
+}
+
+
+
+function kliknutiNaOdpoved() {
+
+  const vsechnyOdpovedi = document.querySelectorAll('#odpovedi');
+
+  vsechnyOdpovedi.forEach((odpoved) => {
+
+    odpoved.addEventListener('click', (vyhodnot));
+
+  })
+
+}
+
+function vyhodnot(odpoved) {
+  let i = 0;
+
+  let pocetSpravnychOdpovedi = 0;
+  let vypocetUspesnosti = 0;
+
+  let odpovezeno = odpoved.target;
+  let indexKliknuteOdpovedi = parseInt(odpovezeno.dataset.odpoved);
+  let indexSpravneOdpovedi = parseInt(poleOtazek[i].indexSpravneOdpovedi);
+  let vnitrniTextOdpovedi = odpovezeno.dataset.odpoved.innerText;
+
+  console.log('vnitrni text: ' + vnitrniTextOdpovedi);
+  console.log('klikla jsem na: ' + indexKliknuteOdpovedi + ' a spravna odpoved je: ' + indexSpravneOdpovedi);
+
+  let kliknuteOdpovedi = [];
+  kliknuteOdpovedi.push = vnitrniTextOdpovedi;
+
+  if (indexKliknuteOdpovedi === indexSpravneOdpovedi) {
+
+    pocetSpravnychOdpovedi++;
+    vypocetUspesnosti = pocetSpravnychOdpovedi / parseInt(poleOtazek.length);
+    console.log('spravna odpoved ' + pocetSpravnychOdpovedi + ' uspesnost ' + vypocetUspesnosti);
+
+  } else console.log('spatna odpoved ' + ' uspesnost ' + vypocetUspesnosti);
+
+
+  iteruj(i);
+
+
+}
 
